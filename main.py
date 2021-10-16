300744ca5e268e6f5788771b42bec2c27d278e0c
lannguyen <18120051@student.hcmus.edu.vn>
2021-10-17 00:01:38 +0700

    add wiki api to main

--- main.py
+++ main.py
@@ -12,13 +12,15 @@ from apis.translate.translate_facade import TranslateFacade
 from apis.location_search.location_search_helper import LocationSearchHelper
 from apis.location_search.location_search_facade import LocationSearchFacade
 
+from apis.wiki_search.wiki_search_helper import WikiHelper
+from apis.wiki_search.wiki_search_facade import WikiFacade
+
 from configs.config import *
 from flask import Flask, Response
 from slackeventsapi import SlackEventAdapter
 from dotenv import load_dotenv
 from pathlib import Path
 from utils import *
-
 
 
 env_path = Path('.') / '.env'
@@ -50,6 +52,10 @@ translate_facade = TranslateFacade()
 location_search_helper = LocationSearchHelper()
 location_search_facade = LocationSearchFacade()
 
+wiki_helper = WikiHelper()
+wiki_facade = WikiFacade()
+
+
 @slack_event_adapter.on('message')
 # Handle all message events
 def handle_message(payload):
@@ -58,7 +64,6 @@ def handle_message(payload):
     user_id = event.get('user')
     channel_id = event.get('channel')
     text = event.get('text')
-
 
     default_message = "I'm sorry. I don't understand."
 
@@ -76,26 +81,26 @@ def handle_message(payload):
 
             country = user_response[1] if len(user_response) > 1 else None
             language = user_response[2] if len(user_response) > 2 else None
-            query = user_response[3]  if len(user_response) > 3 else None
+            query = user_response[3] if len(user_response) > 3 else None
 
             query_helper = QueryHelper(query=query,
-                                        category=category,
-                                        country=country, 
-                                        language=language,
-                                        slack_channel=channel_id)
+                                       category=category,
+                                       country=country,
+                                       language=language,
+                                       slack_channel=channel_id)
 
             result = news_api_helper.get_top_headlines(query_helper)
             logging.info(f"Retrieved {result['totalResults']} results.")
             articles = result["articles"]
-            
+
             news_facade.send_messages(
                 articles,
                 name="Hot News",
                 channel=channel_id
-            )            
-            
+            )
+
             return Response(), 200
-        
+
         elif "/covid" in text[:6].lower():
             user_response = text[6:].split(", ")
             countries = []
@@ -105,7 +110,7 @@ def handle_message(payload):
             else:
                 for i in range(1, len(user_response)):
                     countries.append(str(user_response[i]))
-            
+
             print('Countries: ', countries)
 
             responses, files = [], []
@@ -113,11 +118,12 @@ def handle_message(payload):
                 response = covid_api_healper.get_summary(country)
                 responses.append(response)
                 files.append(f'./images/{country}.png')
-            logging.info(f"Retrieved {len(responses)} responses from {len(countries)} countries.")
+            logging.info(
+                f"Retrieved {len(responses)} responses from {len(countries)} countries.")
 
             covid_facade.send_messages(
-                responses, 
-                files, 
+                responses,
+                files,
                 channel=channel_id
             )
 
@@ -126,18 +132,19 @@ def handle_message(payload):
         elif '/translate' in text[:10].lower():
             user_response = text[10:].split(", ")
 
-            assigned_language = user_response[0] # language you want translate to
-            text = user_response[1] # text of any languages that you want to translate
+            # language you want translate to
+            assigned_language = user_response[0]
+            # text of any languages that you want to translate
+            text = user_response[1]
 
             language_code = DEFAULT_LANGUAGE_CODE
             for language_tuple in LANGUAGES:
-                if language_tuple[1] == assigned_language:
+                if language_tuple[1].lower() == assigned_language:
                     language_code = language_tuple[0]
 
             response = translate_api_helper.do_command(text, language_code)
 
             translate_facade.send_messages(response, channel=channel_id)
-
 
             return Response(), 200
 
@@ -150,7 +157,27 @@ def handle_message(payload):
             term = user_response[0]
             result = location_search_helper.search(term, location)
 
-            location_search_facade.send_messages(result, location, channel=channel_id)            
+            location_search_facade.send_messages(
+                result, location, channel=channel_id)
+
+            return Response(), 200
+
+        elif '/wiki' in text[:5].lower():
+            user_response = text[5:].split(", ")
+
+            # language you want translate to
+            assigned_language = user_response[0]
+            # text of any languages that you want to translate
+            text = user_response[1]
+
+            language_code = DEFAULT_LANGUAGE_CODE
+            for language_tuple in LANGUAGES:
+                if language_tuple[1].lower() == assigned_language:
+                    language_code = language_tuple[0]
+
+            response = wiki_helper.do_command(text, language_code)
+
+            wiki_facade.send_messages(response, channel=channel_id)
 
             return Response(), 200
 
@@ -160,6 +187,7 @@ def handle_message(payload):
 
             response = show_commands(user_id)
             news_facade.client.chat_postMessage(channel=channel_id, **response)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
