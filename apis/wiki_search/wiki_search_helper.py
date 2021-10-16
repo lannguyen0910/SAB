c01c19a038039616f3731941fe48a17b6beea6b4
lannguyen <18120051@student.hcmus.edu.vn>
2021-10-16 23:42:52 +0700

    add wiki-search api helper

---
+++ apis/wiki_search/wiki_search_helper.py
@@ -0,0 +1,51 @@
+import wikipedia
+import logging
+
+
+class WikiHelper:
+    """
+    Search query from wikipedia
+    https://github.com/goldsmith/Wikipedia
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def do_command(self, command, language_code):
+        """
+        Execute command
+        """
+
+        wikipedia.set_lang(language_code)
+        # Ex: wikipedia.set_lang("vi")
+
+        response = None
+
+        # Example call: $wiki python programming
+        query = command.split('wiki')[-1].lstrip().rstrip()
+
+        try:
+            response = wikipedia.summary(query, sentences=5)
+            # page = wikipedia.page(query)
+            # page.url
+            # page.images[0]
+            logging.info(f'{response}')
+
+        except wikipedia.exceptions.DisambiguationError as e:
+            response = "Try one of these options: \n"
+            for idx, opt in enumerate(e.options[:5]):
+                response += f"{idx}. {opt}\n"
+            logging.error(f'{response}')
+
+        except wikipedia.exceptions.PageError as e:
+            response = f"{query} not found on Wikipedia"
+            logging.error(f'{response}')
+
+        return response
+
+
+if __name__ == '__main__':
+    wiki = WikiHelper()
+    text = ' /wiki python programming'
+    response = wiki.do_command(text, 'vi')
+    print(response)
