8159b6043935bae4e72713930530ce3364bc4328
lannguyen <18120051@student.hcmus.edu.vn>
2021-10-31 17:39:03 +0700

    fix errors in main

--- main.py
+++ main.py
@@ -22,13 +22,8 @@ from apis.text_generation.gpt2_facade import GPT2Facade
 from configs.config import *
 from flask import Flask, Response
 from slackeventsapi import SlackEventAdapter
-from dotenv import load_dotenv
-from pathlib import Path
 from utils import *
 
-
-env_path = Path('.') / '.env'
-load_dotenv(dotenv_path=env_path)
 
 app = Flask(__name__)
 
@@ -75,12 +70,12 @@ def handle_message(payload):
 
     default_message = "I'm sorry. I don't understand."
 
-    if user_id != None and news_facade.BOT_ID != user_id:
+    if event.get('bot_id') is None and user_id != None and news_facade.BOT_ID != user_id:
         if text.lower() == 'hello' or text.lower() == 'hi':
             response = show_commands(user_id)
             news_facade.client.chat_postMessage(channel=channel_id, **response)
 
-        elif "#news" in text[:5].lower():
+        elif "$news" in text[:5].lower():
             user_response = text[5:].split(", ")
             category = None
 
@@ -109,14 +104,14 @@ def handle_message(payload):
 
             return Response(), 200
 
-        elif "#covid" in text[:6].lower():
-            user_response = text[6:].split(", ")
+        elif "$covid" in text[:6].lower():
+            user_response = text[7:].split(", ")
             countries = []
             if len(user_response) == 0:
                 country = 'global'
                 countries.append(country)
             else:
-                for i in range(1, len(user_response)):
+                for i in range(0, len(user_response)):
                     countries.append(str(user_response[i]))
 
             print('Countries: ', countries)
@@ -126,6 +121,7 @@ def handle_message(payload):
                 response = covid_api_healper.get_summary(country)
                 responses.append(response)
                 files.append(f'./images/{country}.png')
+
             logging.info(
                 f"Retrieved {len(responses)} responses from {len(countries)} countries.")
 
@@ -137,8 +133,8 @@ def handle_message(payload):
 
             return Response(), 200
 
-        elif '#translate' in text[:10].lower():
-            user_response = text[10:].split(", ")
+        elif '$translate' in text[:10].lower():
+            user_response = text[11:].split(", ")
 
             # language you want translate to
             assigned_language = user_response[0]
@@ -149,15 +145,15 @@ def handle_message(payload):
             for language_tuple in LANGUAGES:
                 if language_tuple[1].lower() == assigned_language:
                     language_code = language_tuple[0]
-
+            
             response = translate_api_helper.do_command(text, language_code)
 
             translate_facade.send_messages(response, channel=channel_id)
 
             return Response(), 200
 
-        elif "#search" in text[:7].lower():
-            user_response = text[7:].split(", ")
+        elif "$search" in text[:7].lower():
+            user_response = text[8:].split(", ")
             location = None
             if len(user_response) > 1:
                 location = user_response[1]
@@ -170,8 +166,8 @@ def handle_message(payload):
 
             return Response(), 200
 
-        elif '#wiki' in text[:5].lower():
-            user_response = text[5:].split(", ")
+        elif '$wiki' in text[:5].lower():
+            user_response = text[6:].split(", ")
 
             # language you want translate to
             assigned_language = user_response[0]
@@ -189,14 +185,14 @@ def handle_message(payload):
 
             return Response(), 200
 
-        elif '#gpt2' in text[:5].lower():
+        elif '$gpt2' in text[:5].lower():
             query = text.split('gpt2')[-1].lstrip().rstrip()
             gpt2_facade.send_messages(query, channel=channel_id)
 
             return Response(), 200
 
-        elif '#overflow' in text[:9].lower():
-            query = text[10:]
+        elif '$overflow' in text[:9].lower():
+            query = text.split('overflow')[-1].lstrip().rstrip()
             print('Overflow query: ', query)
             overflow_facade.send_messages(query, channel=channel_id)
 
