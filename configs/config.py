aeb11aab4e145384cfdbb2c7e559f3b350cbfa1c
lannguyen <18120051@student.hcmus.edu.vn>
2021-10-31 16:01:28 +0700

    fix config get from env

--- configs/config.py
+++ configs/config.py
@@ -1,6 +1,10 @@
 import os
-
+from dotenv import load_dotenv
+from pathlib import Path
 # --------------------------------- NEWS ------------------------------------------
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
 
 SIGNING_SECRET = os.environ.get("SIGNING_SECRET")
 NEWS_API_KEY = os.environ.get("NEWS_API_KEY")
@@ -23,9 +27,9 @@ SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
 CLIENT_SECRET_FILE = 'token.json'
 
 # -------------------------------- LOCATION SEARCH ---------------------------------
-DEFAULT_LIMIT = os.environ['DEFAULT_LIMIT']  # Ex: 5
-DEFAULT_LOCATION = os.environ['DEFAULT_LOCATION']  # Ex: Berlin, Singapore,...
-YELP_API_KEY = os.environ['YELP_API_KEY']
+DEFAULT_LIMIT = os.environ.get('DEFAULT_LIMIT')  # Ex: 5
+DEFAULT_LOCATION = os.environ.get('DEFAULT_LOCATION')  # Ex: Berlin, Singapore,...
+YELP_API_KEY = os.environ.get('YELP_API_KEY')
 
 # -------------------------------------GMAIL ---------------------------------
 GMAIL_CLIENT_SECRET_FILE = 'default_cs.json'
